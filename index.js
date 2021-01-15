029f2a877d551092783cc1364e05feaf468857e4
Emanuel Cortes <emanuelcodes@gmail.com>
2021-01-15 11:58:30 -0500

    [html/css] Eddit appearence of site.

--- index.js
+++ index.js
@@ -39,59 +39,6 @@ fetch('data.json').then(response => response.json()).then(data => {
 
 });
 
-/*
-  At the moment, I can pull data from the JSON file and show it on the HTML.
-  I will figure out how to create a reusable HTML template to pass the
-  information over instead of building one by one.
-*/
-
-//===========================================================================
-
-/*
-#### Study this can help with the template.
-
-#### This would go in the HTML
-<template id="comment-template">
-  <li class="comment">
-    <div class="comment-author"></div>
-    <div class="comment-body"></div>
-    <div class="comment-actions">
-      <a href="#reply" class="reply">Reply</a>
-    </div>
-  </li>
-</template>
-
-
-#### This would be in the JS
-// An array of comments.
-var comments = [
-  {'author': 'Joe', 'body': 'I love this product.'},
-  {'author': 'Mary', 'body': 'Great idea. I have got to get me one of these!'},
-  {'author': 'Eric', 'body': 'These things are fantastic. I bought three.'}
-];
-
-// Get a reference to the comments list in the main DOM.
-var commentsList = document.getElementById('comments');
-
-// Loop through each of the comments and add them to the comments list.
-for (var i = 0; i < comments.length; i++) {
-  var comment = comments[i];
-  var tmpl = document.getElementById('comment-template').content.cloneNode(true);
-  tmpl.querySelector('.comment-author').innerText = comment.author;
-  tmpl.querySelector('.comment-body').innerText = comment.body;
-  commentsList.appendChild(tmpl);
-}
-
-*/
-
-/*
-// On click card it will toggle between showing or hidding the div containing
-// the class='card_lower_body'.
-$('.card').on('click', function () {
-  $(this.getElementsByClassName('card_lower_body')[0]).slideToggle();
-})
-*/
-
 $('.card').on('click', function () {
   $(this.getElementsByClassName('card_lower_body')[0]).slideToggle();
 })
